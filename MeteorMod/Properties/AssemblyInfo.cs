38a34172c81b9f8dad6f0dff4c7c762abdc9ef29
Efe Boyaci <efeboyaci@gmail.com>
2024-06-29 16:21:42 +0300

    Add automatic build package

--- MeteorMod/Properties/AssemblyInfo.cs
+++ MeteorMod/Properties/AssemblyInfo.cs
@@ -6,7 +6,8 @@
         typeof(MeteorMod.MeteorMod),
         "MeteorMod",
         "0.1.0",
-        "Comahar"
+        "Comahar",
+        "https://github.com/Comahar/MeteorMod"
     )
 ]
 [assembly: MelonGame("KO_OP", "Goodbye Volcano High")]
